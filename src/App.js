f25d6b078965ae4a4822078f71b0ef801919534a
Erik Stackelberg <stackelberg@splitstockholm.se>
2019-04-05 09:38:01 +0200

    Set up deploy to GH-pages and routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import AppComponent from './components/App'
 
 class App extends Component {
   render() {
     return (
-      <div>
-      <h1 className="f1 red"> Unsettler </h1>
-      </div>
+      <Router basename={process.env.PUBLIC_URL}>
+        <Route path="/" exact component={AppComponent}/>
+      </Router>
     );
   }
 }
---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={}
+  }
+
+  render() {
+    return (<div className="tc dark-red f1 sans-serif pv6 b">Unsettler</div>);
+  }
+}
