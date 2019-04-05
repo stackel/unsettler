0b074b07a24e9d7ec2a1a7a30c87c62a35b8dfc8
Erik Stackelberg <stackelberg@splitstockholm.se>
2019-04-05 10:57:15 +0200

    more mocked data

--- src/resources/mockedData.js
+++ src/resources/mockedData.js
@@ -18,4 +18,16 @@ export const vacancies = [
     employer: 'Arbetsförmedlingen',
     municipality: 'Jönköping',
   },
+  {
+    id: '9812389444234',
+    title: 'Service-minded Fönsterputsare',
+    employer: 'Hack for Sweden',
+    municipality: 'Stockholm',
+  },
+  {
+    id: '98123893434444234',
+    title: 'Stresstålig abonnemangsförsäljare',
+    employer: 'Arbetsförmedlingen',
+    municipality: 'Luleå',
+  },
 ];
