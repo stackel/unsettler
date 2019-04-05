9d8655f9cd094ca1b97062cedf876c3572b0a4e9
Erik Stackelberg <stackelberg@splitstockholm.se>
2019-04-05 11:36:49 +0200

    hook up to api

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 /* eslint-disable import/no-extraneous-dependencies */
 // import PropTypes from 'prop-types';
 import Swing from 'react-swing';
+import axios from 'axios';
 
 import { vacancies } from '../resources/mockedData';
 // import Card from './Card';
@@ -11,16 +12,43 @@ export default class Stack extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      stack: vacancies,
+      stack: [],
       thrownCards: [],
     };
+  }
+
+  componentDidMount = () => {
+    this.getNewVacations();
+  }
+
+  getNewVacations = () => {
+    /* {
+    recommendedJobs(ratings: [{jobId: "sfa", rating: THUMBS_DOWN},
+     {jobId: "sfa", rating: THUMBS_DOWN}]) {
+    workId
+    headline
+    companyName
+    }
+  } */
+    const { thrownCards } = this.state;
+
+    console.log(thrownCards.map(thrownCard => ({ id: thrownCard.workId })));
+
+    const query = '{ recommendedJobs(ratings: []) { workId headline companyName}}';
+    axios.post('https://unsettler.azurewebsites.net/graphql', { query }).then((response) => {
+      this.setState({
+        stack: response.data.data.recommendedJobs,
+      });
+    }, (error) => {
+      console.error(error);
+    });
   }
 
   onThrowout = (e) => {
     console.log(e);
     const { stack } = this.state;
     const stackCopy = [...stack];
-    const index = stackCopy.map(vacancy => vacancy.id).indexOf(e.target.id);
+    const index = stackCopy.map(vacancy => vacancy.workId).indexOf(e.target.id);
 
     if (index !== -1) {
       stackCopy.splice(index, 1);
@@ -31,51 +59,61 @@ export default class Stack extends Component {
           { vacancy: stack[index], direction: e.throwDirection }],
       }));
     }
+
+    if (stackCopy.length <= 0) {
+      this.getNewVacations();
+    }
   }
 
   render() {
     const { stack, thrownCards } = this.state;
+    if (!stack.length) {
+      return <p> loading</p>;
+    }
     return (
       <div>
+        {
+        stack.length && (
         <Swing
           className="relative center"
           throwout={this.onThrowout}
           config={{
             throwOutConfidence: (xOffset, yOffset, element) => {
               const xConfidence = Math.min(Math.abs(xOffset) / element.offsetWidth
-             + THROW_SENSITIVITY, 1);
+                   + THROW_SENSITIVITY, 1);
               const yConfidence = Math.min(Math.abs(yOffset) / element.offsetHeight
-             + THROW_SENSITIVITY, 1);
+                   + THROW_SENSITIVITY, 1);
 
               return Math.max(xConfidence, yConfidence);
             },
           }}
         >
           {
-          stack.map(vacancy => (
-            <div
-              id={vacancy.id}
-              className="absolute w-100 ba bg-white pv6 ph4"
-              key={vacancy.id}
-            >
-              <span className="db f3 sans-serif">
-                {vacancy.title}
-              </span>
-              <span className="db f4 sans-serif">
-                {vacancy.employer}
-              </span>
-              <span className="db f4 dark-gray sans-serif">
-                {vacancy.municipality}
-              </span>
-            </div>
-          ))
-        }
+                stack.map(vacancy => (
+                  <div
+                    id={vacancy.workId}
+                    className="absolute w-100 ba bg-white pv6 ph4"
+                    key={vacancy.workId}
+                  >
+                    <span className="db f3 sans-serif">
+                      {vacancy.headline}
+                    </span>
+                    <span className="db f4 sans-serif">
+                      {vacancy.companyName}
+                    </span>
+                  </div>
+                ))
+              }
         </Swing>
+        )
+
+      }
+
         <div className="absolute bottom-0">
           {
             thrownCards.map(card => (
-              <div key={card.vacancy.id}>
-                <span className="f5 sans-serif">{card.vacancy.title}</span>
+              <div key={card.vacancy.workId}>
+                <span className="f5 sans-serif">{card.vacancy.headline}</span>
                 <span className="f5 sans-serif">
                   {`  You Swiped ${card.direction.toString().slice(-6)}`}
                 </span>
